d34c2c666ba66658313b45839c84c77b9f9cb655
Nick Banford <nickbanford@gmail.com>
2015-05-05 22:15:38 +0100

    Adds property controller and access for sellers

---
+++ OrangeBricks.Web/Controllers/PropertyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrangeBricks.Web.Controllers
+{
+    public class PropertyController : Controller
+    {
+        [Authorize(Roles = "Seller")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ OrangeBricks.Web/Models/Property.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrangeBricks.Web.Models
+{
+    public class Property
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+    }
+}

a0aa779f42835bff46375b09abe1157043b88254
Nick Banford <nickbanford@gmail.com>
2015-05-05 22:27:01 +0100

    Adds the create property listing form

---
+++ OrangeBricks.Web/Controllers/Property/PropertyController.cs
@@ -0,0 +1,24 @@
+﻿using OrangeBricks.Web.Controllers.Property.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrangeBricks.Web.Controllers
+{
+    public class PropertyController : Controller
+    {
+        [Authorize(Roles = "Seller")]
+        public ActionResult Create()
+        {
+            var viewModel = new CreatePropertyViewModel();
+
+            viewModel.PossiblePropertyTypes = new string[] { "House", "Flat", "Bungalow" }
+                .Select(x => new SelectListItem { Value = x, Text = x })
+                .AsEnumerable();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ OrangeBricks.Web/Controllers/Property/ViewModels/CreatePropertyViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrangeBricks.Web.Controllers.Property.ViewModels
+{
+    public class CreatePropertyViewModel
+    {
+        [Required]
+        [Display(Name = "Property type" )]
+        public string PropertyType { get; set; }
+
+        [Required]
+        [Display(Name = "Street name")]
+        public string StreetName { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Number of bedrooms")]
+        public int NumberOfBedrooms { get; set; }
+
+        public IEnumerable<System.Web.Mvc.SelectListItem> PossiblePropertyTypes { get; set; }
+    }
+}

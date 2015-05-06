fb3fc4b730c86dd45c29a751e570a3e2d8c71c2f
Nick Banford <nickbanford@gmail.com>
2015-05-06 06:55:06 +0100

    Adds required dependencies for testing

---
+++ OrangeBricks.Web.Tests/Controllers/Property/Commands/CreatePropertyCommandHandlerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using OrangeBricks.Web.Models;
+
+namespace OrangeBricks.Web.Tests.Controllers.Property.Commands
+{
+    [TestFixture]
+    public class CreatePropertyCommandHandlerTest
+    {
+        [Test]
+        public void HandleShouldAddProperty()
+        {
+            // Arrange
+            var context = Substitute.For<IOrangeBricksContext>();
+            context.Properties.Returns(Substitute.For<IDbSet<Models.Property>>());
+            var handler = new CreatePropertyCommandHandler(context);
+
+            var command = new CreatePropertyCommand();
+
+            // Act
+            handler.Handle(command);
+
+            // Assert
+            context.Properties.Received(1).Add(Arg.Any<Models.Property>());
+        }
+    }
+
+    public class CreatePropertyCommandHandler
+    {
+        public CreatePropertyCommandHandler(IOrangeBricksContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(CreatePropertyCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CreatePropertyCommand
+    {
+    }
+}

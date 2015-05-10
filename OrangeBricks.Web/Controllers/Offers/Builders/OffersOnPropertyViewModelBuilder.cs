5dc2a943124eb9372576655784c6ee0925576488
Nick Banford <nickbanford@gmail.com>
2015-05-10 16:54:06 +0100

    Adds eager loading of offers for seller view

--- OrangeBricks.Web/Controllers/Offers/Builders/OffersOnPropertyViewModelBuilder.cs
+++ OrangeBricks.Web/Controllers/Offers/Builders/OffersOnPropertyViewModelBuilder.cs
@@ -1,4 +1,8 @@
-﻿using OrangeBricks.Web.Models;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using OrangeBricks.Web.Controllers.Offers.ViewModels;
+using OrangeBricks.Web.Models;
 
 namespace OrangeBricks.Web.Controllers.Offers.Builders
 {
@@ -13,7 +17,24 @@ namespace OrangeBricks.Web.Controllers.Offers.Builders
 
         public OffersOnPropertyViewModel Build(int id)
         {
-            return new OffersOnPropertyViewModel();
+            var property = _context.Properties
+                .Where(p => p.Id == id)
+                .Include(x => x.Offers)
+                .SingleOrDefault();
+
+            var offers = property.Offers ?? new List<Offer>();
+
+            return new OffersOnPropertyViewModel
+            {
+                HasOffers = offers.Any(),
+                Offers = offers.Select(x => new OfferViewModel
+                {
+                    Amount = x.Amount
+                }),
+                PropertyType = property.PropertyType,
+                StreetName = property.StreetName,
+                NumberOfBedrooms = property.NumberOfBedrooms
+            };
         }
     }
 }
--- OrangeBricks.Web/Controllers/Offers/ViewModels/OffersOnPropertyViewModel.cs
+++ OrangeBricks.Web/Controllers/Offers/ViewModels/OffersOnPropertyViewModel.cs
@@ -1,6 +1,18 @@
+using System.Collections.Generic;
+
 namespace OrangeBricks.Web.Controllers.Offers.ViewModels
 {
     public class OffersOnPropertyViewModel
     {
+        public string PropertyType { get; set; }
+        public int NumberOfBedrooms{ get; set; }
+        public string StreetName { get; set; }
+        public bool HasOffers { get; set; }
+        public IEnumerable<OfferViewModel> Offers { get; set; }
+    }
+
+    public class OfferViewModel
+    {
+        public int Amount { get; set; }
     }
 }

018ddf01e04211a3856b37246a6184c4eab716a0
Nick Banford <nickbanford@gmail.com>
2015-05-10 11:39:55 +0100

    Adds a quick view to show sellers properties

--- OrangeBricks.Web/Controllers/Property/PropertyController.cs
+++ OrangeBricks.Web/Controllers/Property/PropertyController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using OrangeBricks.Web.Controllers.Property.Commands;
@@ -44,7 +45,50 @@ namespace OrangeBricks.Web.Controllers.Property
         [Authorize(Roles = "Seller")]
         public ActionResult MyProperties()
         {
-            return View();
+            var builder = new MyPropertiesViewModelBuilder(_context);
+            var viewModel = builder.Build(User.Identity.GetUserId());
+
+            return View(viewModel);
         }
+    }
+
+    public class MyPropertiesViewModelBuilder
+    {
+        private readonly IOrangeBricksContext _context;
+
+        public MyPropertiesViewModelBuilder(IOrangeBricksContext context)
+        {
+            _context = context;
+        }
+
+        public MyPropertiesViewModel Build(string sellerId)
+        {
+            return new MyPropertiesViewModel
+            {
+                Properties = _context.Properties
+                    .Where(p => p.SellerUserId == sellerId)
+                    .Select(p => new PropertyViewModel
+                    {
+                        StreetName = p.StreetName,
+                        Description = p.Description,
+                        NumberOfBedrooms = p.NumberOfBedrooms,
+                        PropertyType = p.PropertyType
+                    })
+                    .ToList()
+            };
+        }
+    }
+
+    public class PropertyViewModel
+    {
+        public string StreetName { get; set; }
+        public string Description { get; set; }
+        public int NumberOfBedrooms { get; set; }
+        public string PropertyType { get; set; }
+    }
+
+    public class MyPropertiesViewModel
+    {
+        public List<PropertyViewModel> Properties { get; set; }
     }
 }

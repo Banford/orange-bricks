6fdadfb3e7fc340c9172d372698ea78cad93cb2b
Nick Banford <nickbanford@gmail.com>
2015-05-10 16:36:41 +0100

    Adds a new controller for handling offers

---
+++ OrangeBricks.Web/Controllers/Offers/OffersController.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+using OrangeBricks.Web.Models;
+
+namespace OrangeBricks.Web.Controllers.Offers
+{
+    public class OffersController : Controller
+    {
+        private readonly IOrangeBricksContext _context;
+
+        public OffersController(IOrangeBricksContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "Seller")]
+        public ActionResult OnProperty(int id)
+        {
+            var builder = new OffersOnPropertyViewModelBuilder(_context);
+            var viewModel = builder.Build(id);
+
+            return View(viewModel);
+        }
+    }
+
+    public class OffersOnPropertyViewModelBuilder
+    {
+        private readonly IOrangeBricksContext _context;
+
+        public OffersOnPropertyViewModelBuilder(IOrangeBricksContext context)
+        {
+            _context = context;
+        }
+
+        public OffersOnPropertyViewModel Build(int id)
+        {
+            return new OffersOnPropertyViewModel();
+        }
+    }
+
+    public class OffersOnPropertyViewModel
+    {
+    }
+}
